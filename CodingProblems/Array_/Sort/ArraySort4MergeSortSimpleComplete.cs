ede41c1f0b5f7feb8cd7cf3cd236ada7655316c7
devin5885 <devin5885@gmail.com>
2018-04-03 17:54:30 -0700

    Fix Resharper warnings

--- CodingProblems/Array_/Sort/ArraySort4MergeSortSimpleComplete.cs
+++ CodingProblems/Array_/Sort/ArraySort4MergeSortSimpleComplete.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using CodingProblems.Array_.Merge;
+using CodingProblems.Array_.MergeSorted;
 
 namespace CodingProblems.Array_.Sort
 {
