ce69363c7ed50685dfb9e4bc7fa67b6160622cc9
devin5885 <devin5885@gmail.com>
2018-04-19 22:10:41 -0700

    Add initial version of file helpers & tests.

---
+++ CodingProblems/Helpers/File_/FileHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CodingProblems.Helpers.File_
+{
+    /// <summary>
+    /// File helpers.
+    /// </summary>
+    internal static class FileHelpers
+    {
+        /// <summary>
+        /// Writes the specified buffer to the specified file.
+        /// </summary>
+        /// <param name="buffer">The buffer to write.</param>
+        /// <param name="fileName">The filename (and path)</param>
+        /// <returns>The resulting file name (and path)</returns>
+        public static string WriteFileFromBuffer(int[] buffer, string fileName = null)
+        {
+            // Use temp name if not specified.
+            if (string.IsNullOrEmpty(fileName))
+                fileName = Path.GetTempFileName();
+
+            // Write buffer.
+            using (var binWriter = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                foreach (var value in buffer)
+                    binWriter.Write(value);
+            }
+
+            // Return result.
+            return fileName;
+        }
+    }
+}
---
+++ CodingProblems/Helpers/File_/FileHelpersTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingProblems.Helpers.File_
+{
+    /// <summary>
+    /// Tests for FileHelpers
+    /// </summary>
+    [TestClass]
+    public class FileHelpersTests
+    {
+        /// <summary>
+        /// Test FileHelpers.WriteFileFromBuffer with a randomly generated file name.
+        /// </summary>
+        [TestMethod]
+        public void TestWriteFileFromBufferSimpleDefaultFileName()
+        {
+            // Initialize test data.
+            var size = 11;
+            var buffer = new int[size];
+            for (var i = 0; i < size; i++)
+                buffer[i] = i;
+
+            // Write test data to file.
+            var fileName = FileHelpers.WriteFileFromBuffer(buffer);
+
+            // Read the file and check contents.
+            using (var br = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                for (var i = 0; i < size; i++)
+                {
+                    int value = br.ReadInt32();
+                    Assert.AreEqual(i, value);
+                }
+            }
+
+            // Delete test file.
+            File.Delete(fileName);
+            Assert.IsFalse(File.Exists(fileName));
+        }
+
+        /// <summary>
+        /// Test FileHelpers.WriteFileFromBuffer with a file name specified.
+        /// </summary>
+        [TestMethod]
+        public void TestWriteFileFromBufferSimpleSpecifyFileName()
+        {
+            // Initialize test data.
+            var size = 11;
+            var buffer = new int[size];
+            for (var i = 0; i < size; i++)
+                buffer[i] = i;
+
+            // Build file name.
+            var fileName = Path.GetTempFileName();
+
+            // Write test data to file.
+            FileHelpers.WriteFileFromBuffer(buffer, fileName);
+
+            // Read the file and check contents.
+            using (var br = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                for (var i = 0; i < size; i++)
+                {
+                    int value = br.ReadInt32();
+                    Assert.AreEqual(i, value);
+                }
+            }
+
+            // Delete test file.
+            File.Delete(fileName);
+            Assert.IsFalse(File.Exists(fileName));
+        }
+    }
+}
