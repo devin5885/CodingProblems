f55fc88d6f98e04cc9a3cc5d9bc59c28b904c476
devin5885 <devin5885@gmail.com>
2018-04-19 23:18:00 -0700

    Add WriteFileOfInts

--- CodingProblems/Helpers/File_/FileHelpers.cs
+++ CodingProblems/Helpers/File_/FileHelpers.cs
@@ -30,5 +30,48 @@ namespace CodingProblems.Helpers.File_
             // Return result.
             return fileName;
         }
+
+        /// <summary>
+        /// Writes a file of integers with a maximum size of int.Max - 1.
+        /// </summary>
+        /// <param name="fileSizeInts">The size of the file (in 32-bit integers).</param>
+        /// <param name="minValue">The minimum value to put in the file.</param>
+        /// <param name="maxValue">The maximum value to put in the file.</param>
+        /// <param name="fileName">The file name.</param>
+        /// <returns>The resulting file name.</returns>
+        public static string WriteFileOfInts(int fileSizeInts, int minValue = 0, int maxValue = int.MaxValue - 1, string fileName = null)
+        {
+            // Use temp name if not specified.
+            if (string.IsNullOrEmpty(fileName))
+                fileName = Path.GetTempFileName();
+
+            // Start at minimum value.
+            int value = minValue;
+
+            // Create buffer.
+            var buffer = new int[fileSizeInts];
+            for (int i = minValue; i < fileSizeInts; i++)
+            {
+                // Write to buffer.
+                buffer[i] = value;
+
+                // Keep updating start value to get values throughout the range but
+                // don't go over the maximum value.
+                if (value == maxValue)
+                    value = minValue;
+                else
+                    value++;
+            }
+
+            // Write entire buffer.
+            using (BinaryWriter binWriter = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                for (int i = 0; i < buffer.Length; i++)
+                    binWriter.Write(buffer[i]);
+            }
+
+            // Return file name.
+            return fileName;
+        }
     }
 }
--- CodingProblems/Helpers/File_/FileHelpersTests.cs
+++ CodingProblems/Helpers/File_/FileHelpersTests.cs
@@ -72,5 +72,116 @@ namespace CodingProblems.Helpers.File_
             File.Delete(fileName);
             Assert.IsFalse(File.Exists(fileName));
         }
+
+        /// <summary>
+        /// Test FileHelpers.WriteFileOfInts with a randomly generated file name.
+        /// </summary>
+        [TestMethod]
+        public void TestWriteFileOfIntsDefaultFileName()
+        {
+            // Write test data to file.
+            var size = 10;
+            var fileName = FileHelpers.WriteFileOfInts(size, 0, 9);
+
+            // Read the file and check contents.
+            using (var br = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                for (var i = 0; i < size; i++)
+                {
+                    int value = br.ReadInt32();
+                    Assert.AreEqual(i, value);
+                }
+            }
+
+            // Delete test file.
+            File.Delete(fileName);
+            Assert.IsFalse(File.Exists(fileName));
+        }
+
+        /// <summary>
+        /// Test FileHelpers.WriteFileOfInts with a manually specified file name.
+        /// </summary>
+        [TestMethod]
+        public void TestWriteFileOfIntsSpecifyFileName()
+        {
+            // Build file name.
+            var fileName = Path.GetTempFileName();
+
+            // Write test data to file.
+            var size = 10;
+            FileHelpers.WriteFileOfInts(size, 0, 9, fileName);
+
+            // Read the file and check contents.
+            using (var br = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                for (var i = 0; i < size; i++)
+                {
+                    int value = br.ReadInt32();
+                    Assert.AreEqual(i, value);
+                }
+            }
+
+            // Delete test file.
+            File.Delete(fileName);
+            Assert.IsFalse(File.Exists(fileName));
+        }
+
+        /// <summary>
+        /// Test FileHelpers.WriteFileOfInts with a custom minValue and maxValue and a file size larger than the maxValue.
+        /// </summary>
+        [TestMethod]
+        public void TestWriteFileOfIntsDoubleSize()
+        {
+            TestWriteFileOfIntsHelper(25, 1, 9);
+        }
+
+        /// <summary>
+        /// Test FileHelpers.WriteFileOfInts with a file size of ~ 1GB.
+        /// </summary>
+        [TestMethod]
+        public void TestWriteFileOfIntsLargeSize()
+        {
+            TestWriteFileOfIntsHelper(int.MaxValue / 8, 0, int.MaxValue);
+        }
+
+        /// <summary>
+        /// Helper for testing WriteFileOfInts
+        /// </summary>
+        /// <param name="fileSizeInts">fileSize</param>
+        /// <param name="minValue">Minimum value</param>
+        /// <param name="maxValue">Maximum value</param>
+        public void TestWriteFileOfIntsHelper(int fileSizeInts, int minValue, int maxValue)
+        {
+            // Write test data to file.
+            var fileName = FileHelpers.WriteFileOfInts(fileSizeInts, minValue, maxValue);
+
+            // Read the file and check contents.
+            using (var br = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                // Initialize expected value.
+                var valueExpected = 0;
+
+                // Check values in file.
+                for (var i = minValue; i < fileSizeInts; i++)
+                {
+                    // Read.
+                    int value = br.ReadInt32();
+
+                    // Do test.
+                    Assert.AreEqual(valueExpected, value);
+
+                    // Reset to min value.
+                    if (valueExpected == maxValue)
+                        valueExpected = minValue;
+                    else
+                        // Update expected value.
+                        valueExpected++;
+                }
+            }
+
+            // Delete test file.
+            File.Delete(fileName);
+            Assert.IsFalse(File.Exists(fileName));
+        }
     }
 }
