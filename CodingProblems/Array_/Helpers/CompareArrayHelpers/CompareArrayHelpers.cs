bfd449c934ad0f353f93c52664604b4c84bc27c5
DevinEllingson <devin5885@gmail.com>
2021-10-27 12:23:59 -0700

    Add test helpers.

---
+++ CodingProblems/Array_/Helpers/CompareArrayHelpers/CompareArrayHelpers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingProblems.Array_.Helpers.CompareArrayHelpers
+{
+    /// <summary>
+    ///  Helpers to compare arrays.
+    /// </summary>
+    public static class CompareArrayHelpers
+    {
+        /// <summary>
+        ///  Compares arrays that are composed of sub-arrays to determine if the arrays contain the same sub-arrays
+        ///  (in any order). (Sub-arrays can also contain matching elements in any order).
+        /// </summary>
+        /// <typeparam name="T">The type of objects that the array stores.</typeparam>
+        /// <param name="arrayOfArraysExpected">The expected array of arrays.</param>
+        /// <param name="arraysOfArrayActual">The actual array of arrays.</param>
+        /// <returns>True if the arrays match, false otherwise.</returns>
+        public static bool CompareArrays<T>(List<List<T>> arrayOfArraysExpected, List<List<T>> arraysOfArrayActual)
+            where T : IComparable
+        {
+            if (arrayOfArraysExpected.Count != arraysOfArrayActual.Count)
+                return false;
+
+            foreach (var arrayExpected in arrayOfArraysExpected)
+            {
+                if (!ContainsArray(arrayExpected, arraysOfArrayActual))
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///  Determines if an array of arrays contains an array. (The array's elements can match in any order).
+        /// </summary>
+        /// <typeparam name="T">The type of objects that the array stores.</typeparam>
+        /// <param name="arrayToFind">The array to find.</param>
+        /// <param name="arrayOfArraysToSearch">The array of arrays to search.</param>
+        /// <returns>True if the array was found, false otherwise.</returns>
+        public static bool ContainsArray<T>(List<T> arrayToFind, List<List<T>> arrayOfArraysToSearch)
+            where T : IComparable
+        {
+            foreach (var arrayCurrent in arrayOfArraysToSearch)
+            {
+                if (CompareArrays(arrayToFind, arrayCurrent))
+                    return true;
+            }
+
+            // No match.
+            return false;
+        }
+
+        /// <summary>
+        ///  Compares two arrays to determine if they have the same elements (in any order).
+        /// </summary>
+        /// <typeparam name="T">The type of objects that the array stores.</typeparam>
+        /// <param name="array1">The first array.</param>
+        /// <param name="array2">The second array.</param>
+        /// <returns>True if the arrays match, false otherwise.</returns>
+        public static bool CompareArrays<T>(List<T> array1, List<T> array2)
+            where T : IComparable
+        {
+            if (array1.Count != array2.Count)
+                return false;
+
+            var result = array1.Intersect(array2);
+            return result.Count() == array2.Count;
+        }
+    }
+}
---
+++ CodingProblems/Array_/Helpers/CompareArrayHelpers/CompareArrayHelpersTests.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingProblems.Array_.Helpers.CompareArrayHelpers
+{
+    /// <summary>
+    ///  Tests for CompareArrayHelpers
+    /// </summary>
+    [TestClass]
+    public class CompareArrayHelpersTests
+    {
+        // CompareArrays Simple Array.
+
+        /// <summary>
+        /// Array matching same order.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestArrayMatchSameOrder_ShouldReturnTrue()
+        {
+            var array1 = new List<int> { 1, 2, 3 };
+            var array2 = new List<int> { 1, 2, 3 };
+            Assert.IsTrue(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array matching different order.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestArrayMatchDifferentOrder_ShouldReturnTrue()
+        {
+            var array1 = new List<int> { 1, 2, 3 };
+            var array2 = new List<int> { 3, 2, 1 };
+            Assert.IsTrue(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array no match.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestCompareArraysNonMatch_ShouldReturnFalse()
+        {
+            var array1 = new List<int> { 1, 2, 3 };
+            var array2 = new List<int> { 1, 2, 4 };
+            Assert.IsFalse(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array no match, different # of elements (1).
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestCompareArraysNonMatchNumberOfElements1_ShouldReturnFalse()
+        {
+            var array1 = new List<int> { 1, 2 };
+            var array2 = new List<int> { 1, 2, 4 };
+            Assert.IsFalse(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array no match, different # of elements (2).
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestCompareArraysNonMatchNumberOfElements2_ShouldReturnFalse()
+        {
+            var array1 = new List<int> { 1, 2, 4 };
+            var array2 = new List<int> { 1, 2 };
+            Assert.IsFalse(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        // ContainsArray.
+
+        /// <summary>
+        /// ContainsArray, Array found.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestContainsArrayFound_ShouldReturnTrue()
+        {
+            var arrayToFind = new List<int> { 3, 4 };
+            var arrayOfArraysToSearch = new List<List<int>>
+            {
+                new List<int> { 1, 2, 8, 20 },
+                new List<int> { 3, 4 },
+                new List<int> { 5, 6, 7 }
+            };
+            Assert.IsTrue(CompareArrayHelpers.ContainsArray(arrayToFind, arrayOfArraysToSearch));
+        }
+
+        /// <summary>
+        /// ContainsArray, Array not found.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestContainsArrayNotFound_ShouldReturnFalse()
+        {
+            var arrayToFind = new List<int> { 1, 2 };
+            var arrayOfArraysToSearch = new List<List<int>>
+            {
+                new List<int> { 1, 2, 8, 20 },
+                new List<int> { 3, 4 },
+                new List<int> { 5, 6, 7 }
+            };
+            Assert.IsFalse(CompareArrayHelpers.ContainsArray(arrayToFind, arrayOfArraysToSearch));
+        }
+
+        /// <summary>
+        /// Array of arrays matching test - Single element.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestArrayOfArraysMatchSingleElement_ShouldReturnTrue()
+        {
+            var array1 = new List<List<int>>
+            {
+                new List<int> { 1 }
+            };
+            var array2 = new List<List<int>>
+            {
+                new List<int> { 1 }
+            };
+            Assert.IsTrue(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array of arrays no-match test - Single element.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestArrayOfArraysNoMatchSingleElement_ShouldReturnFalse()
+        {
+            var array1 = new List<List<int>>
+            {
+                new List<int> { 1 }
+            };
+            var array2 = new List<List<int>>
+            {
+                new List<int> { 2 }
+            };
+            Assert.IsFalse(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array of arrays matching test - Multiple elements.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestArrayOfArraysMatchMultipleElements_ShouldReturnTrue()
+        {
+            var array1 = new List<List<int>>
+            {
+                new List<int> { 1, 2, 3 }
+            };
+            var array2 = new List<List<int>>
+            {
+                new List<int> { 1, 2, 3 }
+            };
+            Assert.IsTrue(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array of arrays matching test - Multiple elements, multiple arrays.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestArrayOfArraysMatchMultipleElementsMultipleArrays_ShouldReturnTrue()
+        {
+            var array1 = new List<List<int>>
+            {
+                new List<int> { 1, 2, 8, 20 },
+                new List<int> { 3, 4 },
+                new List<int> { 5, 6, 7 }
+            };
+            var array2 = new List<List<int>>
+            {
+                new List<int> { 1, 2, 8, 20 },
+                new List<int> { 3, 4 },
+                new List<int> { 5, 6, 7 }
+            };
+            Assert.IsTrue(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array of arrays no-match test - Single element.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestArrayOfArraysNoMatchMultipleElements_ShouldReturnFalse()
+        {
+            var array1 = new List<List<int>>
+            {
+                new List<int> { 1, 2, 4 }
+            };
+            var array2 = new List<List<int>>
+            {
+                new List<int> { 1, 2, 3 }
+            };
+            Assert.IsFalse(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+
+        /// <summary>
+        /// Array of arrays no match - Multiple elements, multiple arrays.
+        /// </summary>
+        [TestMethod]
+        public void CompareArrayHelpersTestArrayOfArraysNoMatchMultipleElementsMultipleArrays_ShouldReturnFalse()
+        {
+            var array1 = new List<List<int>>
+            {
+                new List<int> { 1, 2, 8, 20 },
+                new List<int> { 3, 4 },
+                new List<int> { 5, 6, 7, 8 }
+            };
+            var array2 = new List<List<int>>
+            {
+                new List<int> { 1, 2, 8, 20 },
+                new List<int> { 3, 4 },
+                new List<int> { 5, 6, 7 }
+            };
+            Assert.IsFalse(CompareArrayHelpers.CompareArrays(array1, array2));
+        }
+    }
+}
